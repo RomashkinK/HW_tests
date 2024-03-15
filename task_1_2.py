2d73c6d39316bd61f24622b00d49e79e911a74e1
RomashkinKirill <romashkin-92@ya.ru>
2024-03-15 23:08:08 +0300

    Initial commit

---
+++ task_1_2.py
@@ -0,0 +1,12 @@
+def vote(votes):
+	# your code
+    count = []
+    for i in votes:
+        count.append(votes.count(i))
+        max_times = max (count)
+        n = count.index(max_times)
+    return (votes[n])
+
+if __name__ == '__main__':
+    print(vote([1,1,1,2,3]))
+    print(vote([1,2,3,2,2]))
